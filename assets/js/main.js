38aac52b7ab63018563a8c45ba41d7639d5d0fda
Mohammad Yousuf <52100471+yousufcs50@users.noreply.github.com>
2025-12-24 22:02:55 +0300

    Add main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
